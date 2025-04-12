40a34f283de3339dd1855c6b002669cb12cd30b0
Bharath Nagesh <nagesh.bharath93@gmail.com>
2025-04-12 09:17:57 -0400

    updates

--- src/lib/components/SocialIcons.svelte
+++ src/lib/components/SocialIcons.svelte
@@ -12,7 +12,7 @@
     </svg>
   </a>
 
-  <a href="https://linkedin.com/in/vidyullathaks" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" class="social-icon" class:light style="--size: {size}px;">
+  <a href="https://linkedin.com/in/vidyullatha-ks" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" class="social-icon" class:light style="--size: {size}px;">
     <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
       <rect x="2" y="9" width="4" height="12"></rect>
